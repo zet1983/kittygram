c28d1ec698cda470383ba678fbfd9e46d4154fc5
Денис С. Зимин <zetic@rambler.ru>
2022-12-19 22:37:09 +0300

    Спринт 8 Тема 1 Урок 7

--- cats/views.py
+++ cats/views.py
@@ -1,19 +1,20 @@
-from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 
 from .models import Cat
 from .serializers import CatSerializer
 
 
-@api_view(['GET', 'POST'])
-def cat_list(request):
-    if request.method == 'POST':
+class APICat(APIView):
+    def get(self, request):
+        cats = Cat.objects.all()
+        serializer = CatSerializer(cats, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
         serializer = CatSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.data, status=status.HTTP_201_CREATED) 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    cats = Cat.objects.all()
-    serializer = CatSerializer(cats, many=True)
-    return Response(serializer.data)
--- kittygram/urls.py
+++ kittygram/urls.py
@@ -1,9 +1,7 @@
 from django.urls import include, path
 
-from cats.views import cat_list
+from cats.views import APICat
 
 urlpatterns = [
-   path('cats/', cat_list),
-]
-
-
+    path('cats/', APICat.as_view()),
+] 
